5108ae1839b672ad70e357af5206d69b4ff3a34f
Brandon <bran2miz@gmail.com>
2024-04-12 11:07:06 -0700

    changed middleware so that it is similar to auth-api lab

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -10,23 +10,15 @@ const Collection = require('./collection.js');
 const POSTGRES_URI =
   process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
 
-  if (!POSTGRES_URI) {
-    console.error('DATABASE_URL not set');
-    process.exit(1); // Exit the process with an error code
-  }
-
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // This is important for some hosted databases like Heroku Postgres
-    }
+
   },
 } : {};
 
 // Hooks
 // sequelize allows us to interact with the user model before adding data to the database using the beforeCreate hook. 
-const sequelizeDatabase = new Sequelize(POSTGRES_URI, DATABASE_CONFIG);
+const sequelizeDatabase = new Sequelize(POSTGRES_URI);
 const todoModel = todo(sequelizeDatabase, DataTypes);
 const todoCollection = new Collection(todoModel);
 
